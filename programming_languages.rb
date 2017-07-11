8690998153930703568104db5fb71f742c99bd99
Joe Cha <josephmcha@gmail.com>
2017-07-10 20:32:32 -0400

    Done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
 def reformat_languages(languages)
   # your code here
+new_hash = Hash.new(0)
+languages.each do |style, name|
+  name.each do |lang, type|
+    type.each do |inside, value|
+##binding.pry
+if !new_hash.has_key?(lang)
+
+      new_hash.merge!(lang => {inside => value, :style => [style]})
+else
+##binding.pry
+
+  new_hash[lang][:style] << style
+end
+    end
+  end
+end
+new_hash
 end
